672c2ff333e89c54997df1710007d5ed15af1489
Brandon <bwwmb6@mst.edu>
2020-01-14 17:04:09 -0600

    init commit

--- src/js/library/moire.js
+++ src/js/library/moire.js
@@ -1,7 +1,8 @@
-//! https://medium.com/front-end-weekly/learning-the-p5-canvas-drawing-library-in-es6-and-webpack-bf514a679544
-//! https://github.com/eatspaint/generative/blob/master/js/sketches/moire.js
+//! Original tutorial: https://medium.com/front-end-weekly/learning-the-p5-canvas-drawing-library-in-es6-and-webpack-bf514a679544
+//! Grouping into multiple functions: https://github.com/eatspaint/generative/blob/master/js/sketches/moire.js
 
 export default p => {
+  //* Creating the main canvas
   const canvas = () => {
     p.createCanvas(p.windowWidth, p.windowHeight * 0.9);
     p.imageMode(p.CENTER);
@@ -11,12 +12,14 @@ export default p => {
     p.background(0);
   };
 
+  //* Creating the graphics properties
   const graphics = gfx => {
     gfx.colorMode(gfx.HSL);
-    gfx.stroke(150, 35, 60);
-    gfx.strokeWeight(2);
+    gfx.stroke("#5bffce");
+    gfx.strokeWeight(3);
   };
 
+  //* Creating the points on the graphics "object"
   const points = (gfx, count) => {
     for (let i = 0; i < count; i++) {
       let x = gfx.random(0, gfx.windowWidth + 1);
@@ -25,21 +28,20 @@ export default p => {
     }
   };
 
+  //* Only run once. Calls initializing functions, generates image, generates rotated image.
   p.setup = () => {
     canvas();
 
     let gfx = p.createGraphics(p.windowWidth, p.windowHeight * 0.9);
 
     graphics(gfx);
-    points(gfx, 1500);
+    points(gfx, 2500);
 
     p.image(gfx, 0, 0);
     p.rotate(1);
     p.image(gfx, 0, 0);
-  };
 
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight * 0.9);
+    p.noLoop(); //* Stops draw function from looping
   };
 
   p.draw = () => {};
