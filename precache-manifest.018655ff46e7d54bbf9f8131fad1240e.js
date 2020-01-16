b5e262ed5483a37865ede4884a39eb3ef588a22f
Brandon Whittle <bwwmb6@mst.edu>
2020-01-15 22:08:03 -0600

    Updates

---
+++ precache-manifest.018655ff46e7d54bbf9f8131fad1240e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c7adad3da7053087a66e40fc88c34b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49751859525822654a2a",
+    "url": "/static/css/main.c9440279.chunk.css"
+  },
+  {
+    "revision": "63a2a2a566f2cc99de87",
+    "url": "/static/js/2.de29b8ef.chunk.js"
+  },
+  {
+    "revision": "86c5b350d6e7dd3fd3bea848754c5b02",
+    "url": "/static/js/2.de29b8ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49751859525822654a2a",
+    "url": "/static/js/main.d931ef03.chunk.js"
+  },
+  {
+    "revision": "40c4dad71a814173443e",
+    "url": "/static/js/runtime-main.8acb781e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9019bc8f5cab93bf4786485fab7c4711.js"
+  "/precache-manifest.018655ff46e7d54bbf9f8131fad1240e.js"
 );
 
 self.addEventListener('message', (event) => {
