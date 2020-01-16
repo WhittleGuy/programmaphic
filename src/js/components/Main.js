f18e92d92da7e778ee34c475248b33a96b350629
Brandon <bwwmb6@mst.edu>
2020-01-16 11:41:19 -0600

    added circles

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -14,7 +14,7 @@ const Main = () => (
     </Route>
     <Route exact path="/moire" component={Moire} />
     <Route path="/sines" component={Sines} />
-    <Route path="/cirgle" component={Circles} />
+    <Route path="/circles" component={Circles} />
     <Route path="/flow" component={Flow} />
     <Route path="/lines" component={Lines} />
     <Route path="/*" component={Moire} />
