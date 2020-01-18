c37c89b18a0c5adcf15b0120b2fbe2a3889f3729
Brandon <bwwmb6@mst.edu>
2020-01-18 10:32:51 -0600

    heptiral centering issues corrected

--- src/js/sketches/heptiral.js
+++ src/js/sketches/heptiral.js
@@ -6,41 +6,59 @@ export default p => {
     p.colorMode(p.RGB);
     p.angleMode(p.DEGREES);
     p.createCanvas(p.windowWidth, p.windowHeight * 0.95);
-    p.frameRate(1);
+    p.frameRate(0.5);
     p.imageMode(p.CENTER);
-    p.translate(p.windowWidth / 2, (p.windowHeight * 0.95) / 2);
     p.smooth();
   };
 
   //! Makes a circle & takes parameters to allow variance in loops
-  p.heptagonSide = (amp, adj) => {
+  const heptagon = (gfx, amp, adj) => {
     let points = [];
     let start = 0;
     //! Creating a heptagon of points
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 7; i++) {
       for (let j = start; j < start + Math.floor(ANG_RES / 7) + adj; j++) {
-        points[j] = new point(
-          p,
-          p.windowWidth / 2 +
-            amp *
-              (1 -
-                (1 - Math.cos(p.TWO_PI / 14)) *
-                  Math.sin(((7 / 2) * j * p.TWO_PI) / 360)) *
-              Math.cos((j * p.TWO_PI) / 360),
-          p.windowHeight / 2 +
-            amp *
-              (1 -
-                (1 - Math.cos(p.TWO_PI / 14)) *
-                  Math.sin(((7 / 2) * j * p.TWO_PI) / 360)) *
-              Math.sin((j * p.TWO_PI) / 360)
-        );
+        if (i % 2 === 0) {
+          points[j] = new point(
+            gfx,
+            gfx.windowWidth / 2 -
+              amp *
+                (1 -
+                  (1 - Math.cos(p.TWO_PI / 14)) *
+                    Math.sin(((7 / 2) * j * p.TWO_PI) / 360)) *
+                Math.cos((j * p.TWO_PI) / 360),
+            (gfx.windowHeight * 0.95) / 2 +
+              amp *
+                (1 -
+                  (1 - Math.cos(p.TWO_PI / 14)) *
+                    Math.sin(((7 / 2) * j * p.TWO_PI) / 360)) *
+                Math.sin((j * p.TWO_PI) / 360)
+          );
+        }
+        if (i % 2 === 1) {
+          points[j] = new point(
+            gfx,
+            gfx.windowWidth / 2 -
+              amp *
+                (1 +
+                  (1 - Math.cos(p.TWO_PI / 14)) *
+                    Math.sin(((7 / 2) * j * p.TWO_PI) / 360)) *
+                Math.cos((j * p.TWO_PI) / 360),
+            (gfx.windowHeight * 0.95) / 2 +
+              amp *
+                (1 +
+                  (1 - Math.cos(p.TWO_PI / 14)) *
+                    Math.sin(((7 / 2) * j * p.TWO_PI) / 360)) *
+                Math.sin((j * p.TWO_PI) / 360)
+          );
+        }
       }
       start += Math.floor(ANG_RES / 7);
     }
 
     //! Connecting all the points
     for (let i = 0; i < points.length - 1; i++) {
-      p.line(
+      gfx.line(
         points[i].pos.x,
         points[i].pos.y,
         points[i + 1].pos.x,
@@ -49,14 +67,8 @@ export default p => {
     }
   };
 
-  p.heptagon = (amp, adj) => {
-    for (let i = 1; i <= 40; i++) {
-      //p.stroke(150, p.random() * 55 + 200, 190);
-      p.stroke(255);
-      p.heptagonSide(amp, adj);
-      p.rotate(360 / 7);
-      p.translate(70, -850);
-    }
+  const graphics = gfx => {
+    gfx.stroke(Math.random() * 255, Math.random() * 255, Math.random() * 255);
   };
 
   //! Onetime setup commands
@@ -67,14 +79,18 @@ export default p => {
   //! Rendering
   p.draw = () => {
     p.background(0);
-    let mult = Math.floor(Math.random() * 50) + 50;
-    let size = Math.floor(Math.random() * 50) + 100;
-    let inc = Math.floor(Math.random() * 3);
-    let rot = Math.floor(Math.random() * 15);
+    let mult = Math.floor(Math.random() * 50) + 10;
+    let size = Math.floor(Math.random() * 50) + 200;
+    let inc = Math.floor(Math.random() * 10 + 5) / 1000;
+    let rot = Math.floor(Math.random() * 45 + 1);
+    let gfx = p.createGraphics(p.windowWidth, p.windowHeight * 0.95);
+    graphics(gfx);
+    p.translate(p.windowWidth / 2, (p.windowHeight * 0.95) / 2);
+    heptagon(gfx, size, 4);
     for (let k = 0; k < mult; k++) {
-      p.heptagon(size, 4);
+      p.image(gfx, 0, 0);
       p.rotate(rot);
-      size += inc;
+      p.scale(1 + inc);
     }
   };
 };
